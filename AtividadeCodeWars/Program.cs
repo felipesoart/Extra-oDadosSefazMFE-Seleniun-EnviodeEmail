44cde4401c50a8294ea79acb53514cfb75c082e6
Felipe Souza <felipesoart@gmail.com>
2022-05-01 15:54:13 -0300

    first commit

---
+++ AtividadeCodeWars/Program.cs
@@ -0,0 +1,191 @@
+﻿using System;
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Interactions;
+using OpenQA.Selenium.Support.UI;
+
+
+namespace AtividadeCodeWars
+{
+    public class Program
+    {
+
+        public static string[] conteudo;
+
+        static void Main(string[] args)
+        {
+
+            
+
+            IWebDriver driver = new ChromeDriver(@"C:\Users\admin\source\repos\AtividadeCodeWars\AtividadeCodeWars\bin\Debug");
+            driver.Navigate().GoToUrl("https://www.sefaz.ce.gov.br/category/informativos-mfe/");
+
+            if (!driver.Title.Equals("Arquivos Informativos MFE - Secretaria da Fazenda"))
+            {
+                throw new Exception("Esta não é a página de entrada");
+            }
+                                              
+
+            foreach (var item in driver.FindElements(By.ClassName("cc-post")))
+            {
+                IWebElement dataPublicacao = item.FindElement(By.ClassName("cc-post-metas-date"));
+                IWebElement descricaoPublicacao = item.FindElement(By.ClassName("cc-post-excerpt"));
+                IWebElement siteReferenciaPublicacao = item.FindElement(By.ClassName("cc-post-title"));
+                siteReferenciaPublicacao.GetAttribute("href");
+                siteReferenciaPublicacao.FindElement(By.TagName("h3"));
+
+                Console.WriteLine("\n" + dataPublicacao.Text + "\n" + siteReferenciaPublicacao.FindElement(By.TagName("h3")).Text + "\n" + descricaoPublicacao.Text + "\n" + siteReferenciaPublicacao.GetAttribute("href") + "\n");
+            }          
+                
+            Console.Read();
+            
+
+            //1 capitura tudas as informações em html
+            //string pagesource = driver.PageSource;
+            //Console.WriteLine(pagesource);
+            //Console.Read();
+
+            //2 IWebElement element = driver.FindElement(By.Id("lst-ib"));//escolhe um elemento da page
+            //element.SendKeys("selenium Testing");//digita no elemento acima
+            //System.Threading.Thread.Sleep(4000);//espera
+            //element.Clear();//apaga o que foi escrito no elemento
+            //Console.Read();
+
+            //3 coleta as referencias da tag A
+            //foreach (var item in driver.FindElements(By.TagName("a")))
+            //{
+            //    Console.WriteLine(item.GetAttribute("href"));
+            //}
+            //Console.Read();
+
+            //4 para clicar em um botao
+            //IWebElement element = driver.FindElement(By.ClassName("cc-post-title"));
+            //System.Threading.Thread.Sleep(5000);
+            //element.Click();
+            //Console.Read();
+
+
+            //5 encontra um elemento pode usar varios BY para verificação
+            //try
+            //{
+            //    driver.FindElement(By.Id("webcha"));
+            //    Console.WriteLine("Elemento encontrado ID");
+
+            //}
+            //catch (Exception e)
+            //{
+            //    Console.WriteLine(e.Message+"elemento não encontrado");                
+            //}
+            //Console.Read();
+
+            //6 traz o titulo da pagira em javascript por meio de execução java script
+            //IJavaScriptExecutor js = driver as IJavaScriptExecutor;
+            //string title = (string)js.ExecuteScript("return document.title");
+            //Console.WriteLine(title);
+            //Console.Read();
+
+            //7 abre uma caixa de alert por meio de execução java script
+            //IJavaScriptExecutor js = driver as IJavaScriptExecutor;
+            //js.ExecuteScript("alert('oi este é o alert');");
+            //System.Threading.Thread.Sleep(6000);
+            //IAlert alert = driver.SwitchTo().Alert();
+            //alert.Accept();            
+            //Console.Read();
+
+
+            //8 volta e avancar da pagina
+            //IWebElement element = driver.FindElement(By.LinkText("Descontinuidade do leiaute 0.07 e liberação do DriverMFE"));//nome do local a que deseja a ação 
+            //element.Click();
+            //System.Threading.Thread.Sleep(4000);
+            //driver.Navigate().Back();//voltar
+            //System.Threading.Thread.Sleep(4000);
+            //driver.Navigate().Forward();//avançar
+
+            //9driver.Manage().Window.Maximize();//deixa a tela grande
+
+            //10driver.Navigate().Refresh();//rezetar page
+
+            //11 em um elemento expecifico clicar com o direito do mouser sobre ele
+            //IWebElement element = driver.FindElement(By.ClassName("cc-post-excerpt"));
+            //System.Threading.Thread.Sleep(4000);
+            //Actions builder = new Actions(driver);
+            //builder.ContextClick(element).Build().Perform();
+
+            //12 decer ou subir o scroll
+            //IJavaScriptExecutor js = driver as IJavaScriptExecutor;
+            //System.Threading.Thread.Sleep(4000);
+            //js.ExecuteScript("window.scrollBy(0,950);");
+            //Console.Read();
+
+            //13 selecionando um valor em um select dropdown list    não funciona no sefaz pq nao tem select        
+            //var option = driver.FindElement(By.Id("my-list"));
+            //System.Threading.Thread.Sleep(4000);
+            //var selectElement = new SelectElement(option);
+            //System.Threading.Thread.Sleep(4000);
+            //selectElement.SelectByText("WEBMAIL");
+            //Console.Read();
+
+            //14 salva o que tela de conteudo na pagina
+            //Screenshot ss = ((ITakesScreenshot)driver).GetScreenshot();
+            //ss.SaveAsFile("C:\\sample.png", ScreenshotImageFormat.Png);
+            //Console.Read(); 
+
+            //15 verificar elemento esta presente
+            //IWebElement element = driver.FindElement(By.ClassName("cc-post-excerpt"));
+            //Console.WriteLine(element.Displayed);
+            //Console.Read();
+
+            //15.1 obtem o valor do elemento
+            //IWebElement element = driver.FindElement(By.ClassName("cc-post-excerpt"));
+            //Console.WriteLine(element.Text);
+            //Console.Read();
+
+            //16 verificar se o titulo e verdadeiro se não for ele diz o titulo que se encontra na pagina
+            //try
+            //{
+            //    Assert.AreEqual("Arquivos Informativos MFE - Secretaria da Fazenda", driver.Title);
+            //    Assert.IsTrue(driver.FindElement(By.ClassName("cc-button")).Displayed); //verifica se existe Displayed=existe na tela Selected = checkbox
+            //    Console.WriteLine("assertion pass");
+            //}
+            //catch (Exception e)
+            //{
+
+            //    Console.WriteLine(e);
+            //}
+
+            //String html_page2 = "Categoria: Informativos MFE";
+            //try
+            //{                
+            //    Assert.IsTrue(driver.FindElement(By.TagName("body")).Text.Contains(html_page2)); //verifica se existe o conteldo text na tagname
+            //    Console.WriteLine("assertion pass");
+            //}
+            //catch (Exception e)
+            //{
+
+            //    Console.WriteLine(e);
+            //}
+
+            //Console.Read();
+
+            //17 clica em um bottao por execução javascript
+            //IWebElement element = driver.FindElement(By.ClassName("cc-button"));
+            //System.Threading.Thread.Sleep(4000);
+            //((IJavaScriptExecutor)driver).ExecuteScript("arguments[0].click();", element);
+
+            //System.Threading.Thread.Sleep(4000);
+            //driver.Quit();
+
+
+        }
+
+
+
+    }
+
+   
+}
+
+
+
+
---
+++ ExtracaoDadosSefaz/Program.cs
@@ -0,0 +1,169 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Net;
+using System.Net.Mail;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExtracaoDadosSefaz
+{
+    public class Program
+    {
+
+        SqlConnection conexao = new SqlConnection();       
+        //string dataColeta = DateTime.Now.ToString("dd-MM-yyyy HH:mm:ss");
+        
+        string tituloEmail = "Novidades Sobre MFE de Sefaz do Ceará. Data Coleta: " + DateTime.Now.ToString("dd-MM-yyyy HH:mm:ss");
+
+        public static string conteudoEmail = string.Empty;
+
+
+        static void Main(string[] args)
+        {
+        
+          IWebDriver driver = new ChromeDriver(@"C:\tfs\Checkout\Source-1.3.1.2\AtividadeCodeWars");
+            driver.Navigate().GoToUrl("https://www.sefaz.ce.gov.br/category/informativos-mfe/");
+
+            if (!driver.Title.Equals("Arquivos Informativos MFE - Secretaria da Fazenda"))
+            {
+                throw new Exception("Esta não é a página de entrada");
+            }
+
+
+            foreach (var item in driver.FindElements(By.ClassName("cc-post")))
+            {
+                IWebElement dataPublicacao2 = item.FindElement(By.ClassName("cc-post-metas-date"));
+                IWebElement descricaoPublicacao2 = item.FindElement(By.ClassName("cc-post-excerpt"));
+                IWebElement siteReferenciaPublicacao = item.FindElement(By.ClassName("cc-post-title"));
+                siteReferenciaPublicacao.GetAttribute("href");
+                siteReferenciaPublicacao.FindElement(By.TagName("h3"));
+
+                string dataPublicacao = dataPublicacao2.Text;
+                string tituloPublicacao = siteReferenciaPublicacao.FindElement(By.TagName("h3")).Text;
+                string descricaoPublicacao = descricaoPublicacao2.Text;
+                string urlPublicacao = siteReferenciaPublicacao.GetAttribute("href");
+
+
+                Program sql = new Program();
+                sql.inserrirDecNoticiasMFESefazCe(dataPublicacao, tituloPublicacao, descricaoPublicacao, urlPublicacao);
+
+                //Console.WriteLine("\n" + dataPublicacao.Text + "\n" + siteReferenciaPublicacao.FindElement(By.TagName("h3")).Text + "\n" + descricaoPublicacao.Text + "\n" + siteReferenciaPublicacao.GetAttribute("href") + "\n");
+
+            }
+
+            Program email = new Program();
+            if (!string.IsNullOrEmpty(conteudoEmail))
+            {
+                email.enviarEmail();
+            }
+           
+            driver.Quit();
+            //Console.Read();
+
+        }
+
+        public void conectar()
+        {
+            conexao.ConnectionString = ConfigurationManager.ConnectionStrings["DefaultConnection"].ToString();
+            try
+            {
+                conexao.Open();
+                //Console.WriteLine("Conexão Aberta");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("\n\nproblema na comunicação com o banco");
+                Console.WriteLine(ex.Message + "\n\n");
+                Console.Read();
+            }            
+
+        }
+        string dataColeta = string.Empty;
+        public void inserrirDecNoticiasMFESefazCe(string dataPublicacao, string tituloPublicacao, string descricaoPublicacao, string urlPublicacao)
+        {
+            conectar();
+            SqlCommand cmd = new SqlCommand();
+            cmd.Connection = conexao;
+            cmd.CommandType = System.Data.CommandType.Text;
+            //cmd.CommandText = string.Format (@"SELECT * FROM DECNOVIDADESMFESEFAZCE (NOLOCK) WHERE URLPUBLICACAO = '{0}'", urlPublicacao);
+            //cmd.CommandText = string.Format(urlPublicacao);
+
+            var reader = verificarURL(urlPublicacao);
+                        
+            if (!reader)
+            {
+
+                cmd.CommandText = string.Format(@"INSERT [dbo].[DECNOVIDADESMFESEFAZCE] ([DATAPUBLICACAO], [DATACRIACAO], [TITULOPUBLICACAO], [DESCRICAOPUBLICACAO],URLPUBLICACAO)
+                                VALUES ('{0}',GETDATE(),'{1}','{2}','{3}')", dataPublicacao, tituloPublicacao, descricaoPublicacao, urlPublicacao);
+
+                cmd.ExecuteNonQuery();
+
+                conteudoEmail = conteudoEmail
+                            + "<br />Data da Publicação: " + dataPublicacao
+                            + "<br />Título da Publicação: " + tituloPublicacao
+                            + "<br />Descrição da Publicação: " + descricaoPublicacao
+                            + "<br />Url da Publicação: " + urlPublicacao + "<br /><br />";
+            }
+
+            conexao.Close();
+            //Console.WriteLine("Conexão fechada" + conteudoEmail);
+        }
+
+        public bool verificarURL(string urlPublicacao)
+        {
+            bool ret = false;
+            using (var conexao = new SqlConnection())
+            {
+
+                conexao.ConnectionString = ConfigurationManager.ConnectionStrings["DefaultConnection"].ConnectionString;
+                conexao.Open();
+                using (var comando = new SqlCommand())
+                {
+                    comando.Connection = conexao;
+                    comando.CommandText = string.Format(@"SELECT * FROM DECNOVIDADESMFESEFAZCE (NOLOCK) WHERE URLPUBLICACAO = '{0}'", urlPublicacao);                    
+
+
+                    var reader = comando.ExecuteReader();
+                    if (reader.Read())
+                    {
+                        ret=true;
+                    }
+                }
+            }
+
+            return ret;
+
+        }
+
+        public void enviarEmail()
+        {
+            
+            var client = new SmtpClient()
+            {
+                Host = ConfigurationManager.AppSettings["EmailServidor"],
+                Port = Convert.ToInt32(ConfigurationManager.AppSettings["EmailPorta"]),
+                EnableSsl = (ConfigurationManager.AppSettings["EmailSsl"] == "S"),
+                UseDefaultCredentials = false,
+                DeliveryMethod = SmtpDeliveryMethod.Network,
+                Credentials = new NetworkCredential(
+                    ConfigurationManager.AppSettings["EmailUsuario"],
+                    ConfigurationManager.AppSettings["EmailSenha"])
+            };
+            var mensagem = new MailMessage();
+            mensagem.From = new MailAddress(ConfigurationManager.AppSettings["EmailOrigem"], "Decodificar LTDA");
+            mensagem.To.Add("felipe.souza@decodificar.net.br, dsscaze@decodificar.net.br");
+            mensagem.Subject = tituloEmail;
+            mensagem.Body = string.Format(conteudoEmail);
+            mensagem.IsBodyHtml = true;
+
+            client.Send(mensagem);
+        }
+       
+    }
+}
